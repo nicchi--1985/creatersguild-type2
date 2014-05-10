9d9ab719c2967338ba6e16b5f0294125d2b348e6
adcregil <adcregil@creatersguild.com>
2014-05-11 02:56:44 +0900

    lets start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creatersguild::Application.config.secret_token = 'd58f6b7585bb1352bfa2eef79f8aca4751f15fa55e882df70e32aa70e1b207d10bdb47261d2ccb22ad75ab62d6b5d90f9c6b167e3a9befe045496516175c6635'
